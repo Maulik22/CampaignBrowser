d46bff1c7ff753c00aa1d1e1e08d23ea2387d820
Maulik Bhuptani <bhuptanimaulikp@gmail.com>
2018-04-24 23:51:23 +0530

    First commit

--- CampaignBrowser/Screens/CampaignsListing/CampaignListingView.swift
+++ CampaignBrowser/Screens/CampaignsListing/CampaignListingView.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+import RxSwift
 
 /**
  The view which displays the list of campaigns. It is configured in the storyboard (Main.storyboard). The corresponding
@@ -22,6 +22,9 @@ class CampaignListingView: UICollectionView {
 
         /** The cell which is used to display a campaign. */
         case campaignCell
+        
+        /** The cell which is used to display an error screen. */
+        case errorCell
     }
 
     /**
@@ -34,6 +37,29 @@ class CampaignListingView: UICollectionView {
         strongDataSource = campaignDataSource
         reloadData()
     }
+    
+    /**
+     Displays loading screen.
+     */
+    func showLoadingScreen() {
+        let loadingDataSource = LoadingDataSource()
+        dataSource = loadingDataSource
+        delegate = loadingDataSource
+        strongDataSource = loadingDataSource
+        reloadData()
+    }
+    
+    /**
+     Displays Error screen when an error is recorded.
+     */
+    func showErrorScreenIn(campaignListingViewController: CampaignListingViewController?) {
+        let errorDataSource = ErrorDataSource(campaignListingViewController: campaignListingViewController)
+        dataSource = errorDataSource
+        delegate = errorDataSource
+        strongDataSource = errorDataSource
+        reloadData()
+    }
+    
 }
 
 
@@ -81,7 +107,6 @@ class ListingDataSource: NSObject, UICollectionViewDataSource, UICollectionViewD
 }
 
 
-
 /**
  The data source for the `CampaignsListingView` which is used while the actual contents are still loaded.
  */
@@ -94,12 +119,59 @@ class LoadingDataSource: NSObject, UICollectionViewDataSource, UICollectionViewD
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let reuseIdentifier = CampaignListingView.Cells.loadingIndicatorCell.rawValue
-        return collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
-                                                  for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        /** Show and animate activityIndicator till we get campaigns list/error */
+        let activityIndicator: UIActivityIndicatorView? = cell.viewWithTag(20) as? UIActivityIndicatorView
+        activityIndicator?.startAnimating()
+
+        return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
         return collectionView.frame.size
     }
+}
+
+
+/**
+ The data source for the `CampaignsListingView` which is used while any kind of error is recorded.
+ */
+class ErrorDataSource: NSObject, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    /** The viewcontroller where error screen needs to be displayed. */
+    let campaignListingViewController: CampaignListingViewController?
+    
+    /**
+     Designated initializer.
+     
+     - Parameter campaignListingViewController:
+     */
+    init(campaignListingViewController: CampaignListingViewController?) {
+        self.campaignListingViewController = campaignListingViewController
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let reuseIdentifier = CampaignListingView.Cells.errorCell.rawValue
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        /** Button which will try to load campaigns list on tap */
+        let button: UIButton? = cell.viewWithTag(22) as? UIButton
+        button?.addTarget(campaignListingViewController, action: #selector(CampaignListingViewController.tryToLoadCampaignList), for: .touchUpInside)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
 }
--- CampaignBrowser/Screens/CampaignsListing/CampaingListingViewController.swift
+++ CampaignBrowser/Screens/CampaignsListing/CampaingListingViewController.swift
@@ -21,14 +21,36 @@ class CampaignListingViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        // Load the campaign list and display it as soon as it is available.
+        
+        tryToLoadCampaignList()
+    }
+    
+    func tryToLoadCampaignList(){
+        
+        /** Show loading screen till we get campaign list. */
+        self.typedView.showLoadingScreen()
+        
+        /** Load the campaign list and display it as soon as it is available. */
         ServiceLocator.instance.networkingService
             .createObservableResponse(request: CampaignListingRequest())
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { [weak self] campaigns in
                 self?.typedView.display(campaigns: campaigns)
-            })
+                },onError: { [weak self] error in
+                    
+                    /** Error codes related to possible network failures ::
+                       -1001 - The request timed out.
+                       -1003 - A server with the specified hostname could not be found.
+                       -1009 - The Internet connection appears to be offline.
+                       -1200 - An SSL error has occurred and a secure connection to the server cannot be made.
+                     */
+                    if (error as NSError).code == -1001 || (error as NSError).code == -1003 || (error as NSError).code == -1009  || (error as NSError).code == -1200 {
+                        self?.typedView.showErrorScreenIn(campaignListingViewController: self)
+                    }
+                    
+                    print("Error recorded :: \(error.localizedDescription)")
+                }
+            )
             .addDisposableTo(disposeBag)
     }
 }
